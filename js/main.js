7a90f12e6bbb2eb3ebac391c470c2ec7f85f7ee0
githubyosshi <Learn Git and GitHub without any code!kanrisya.shisutemu@gmail.com>
2019-02-25 16:53:14 +0900

    Button_three-dimensional_commit

--- js/main.js
+++ js/main.js
@@ -6,4 +6,10 @@
   btn.addEventListener('click',() => {    // クリックしたら
     btn.textContent = 'hit!';             // テキスト変更
   });
+  btn.addEventListener('mousedown', () => {  // 押し込まれたら
+    btn.classList.add('pressed');
+  });
+  btn.addEventListener('mouseup', () => {  // 離したら
+    btn.classList.remove('pressed');       // 元に戻す
+  });
 }
