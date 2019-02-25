32446536ceaeece6336358b62c645b3b22daba94
githubyosshi <Learn Git and GitHub without any code!kanrisya.shisutemu@gmail.com>
2019-02-25 18:53:34 +0900

    Omikuji_with_array_commit

--- js/main.js
+++ js/main.js
@@ -4,20 +4,22 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click',() => {    // クリックしたら
-    const n = Math.floor(Math.random() * 3); // ランダムな数値生成
-    btn.textContent = n;                     // 数値変更表示
+    const results = ['大吉', '中吉', '凶', '末吉'];   // 配列定義
+    const n = Math.floor(Math.random() * results.length); // ランダムな数値生成
+    btn.textContent = results[n];            // n番目を表示
+    // btn.textContent = n;                     // 数値変更表示
     // btn.textContent = 'hit!';             // テキスト変更表示
-    switch (n) {
-      case 0:
-        btn.textContent = '大吉';
-        break;
-      case 1:
-        btn.textContent = '中吉';
-        break;
-      case 2:
-        btn.textContent = '凶';
-        break;
-    }
+    // switch (n) {
+    //   case 0:
+    //     btn.textContent = '大吉';
+    //     break;
+    //   case 1:
+    //     btn.textContent = '中吉';
+    //     break;
+    //   case 2:
+    //     btn.textContent = '凶';
+    //     break;
+    // }
   });
   btn.addEventListener('mousedown', () => {  // 押し込まれたら
     btn.classList.add('pressed');
