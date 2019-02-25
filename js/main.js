6943c15dc217061fe46fa819d5b4a1e4356ba34c
githubyosshi <Learn Git and GitHub without any code!kanrisya.shisutemu@gmail.com>
2019-02-25 17:38:18 +0900

    Random_integer_Omikuji_commit

--- js/main.js
+++ js/main.js
@@ -4,7 +4,20 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click',() => {    // クリックしたら
-    btn.textContent = 'hit!';             // テキスト変更
+    const n = Math.floor(Math.random() * 3); // ランダムな数値生成
+    btn.textContent = n;                     // 数値変更表示
+    // btn.textContent = 'hit!';             // テキスト変更表示
+    switch (n) {
+      case 0:
+        btn.textContent = '大吉';
+        break;
+      case 1:
+        btn.textContent = '中吉';
+        break;
+      case 2:
+        btn.textContent = '凶';
+        break;
+    }
   });
   btn.addEventListener('mousedown', () => {  // 押し込まれたら
     btn.classList.add('pressed');
